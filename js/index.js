deec09962eb8132018485a1d0e3d819a5b4e70fa
Eze Gatica <35713330+ezegatica@users.noreply.github.com>
2021-01-26 20:05:14 -0300

    optimizando el tamaño de los archivos

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
 var na = 0;
 var txt = "EZEQUIEL_GATICA";
 var speed = 150;
+var redes = document.getElementById("redes");
+var espacio = document.getElementById("espacio");
+var navbar = document.getElementById("navbar");
+
 function tW() {
+    redes.style.display = 'none';
+    // navbar.style.display = 'none';
+
     if (na < txt.length) {
         var txtPrint = txt.toUpperCase();
         document.getElementById("text").innerHTML += txtPrint.charAt(na);
         na++;
         setTimeout(tW, speed);
     }
+    setTimeout(() => {
+        showRedes()
+    }, 2500);
 }
-setTimeout(() => {
-    // console.log("Pasaron 4 segundos")
-    $('#redes').show();
-    $('#navbar').show();
-    $('#espacio').hide();
-}, 2500);
+function showRedes() {
+    redes.style.display = 'block';
+    // navbar.style.display = 'block';
+    espacio.style.display = 'none';
+}
